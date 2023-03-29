2fbc3016635427c5b4ac63ce35b6c4bedf24924a
CosyTreq <asriel27@naver.com>
2023-03-29 12:46:03 +0900

    3/29
    프로젝트 구조 갈아엎기, 오류 수정

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from starlette.middleware.cors import CORSMiddleware
 import requests
 import json
 from urllib import parse
 import time
+from pydantic import BaseModel
+
 
 token = ''
 
-url1 = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + parse.quote("T1 Gumayusi")
-response = requests.get(url1, headers={'X-Riot-Token': token})
-
-url2 = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + response.json()['id']
-response2 = requests.get(url2, headers={'X-Riot-Token': token})
-
-puuid = response.json()['puuid']
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173"
-]
+
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,24 +20,44 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["X-Custom-Header"],
+
+
 )
 
+api_router = APIRouter()
 
-@app.get("/info")
-async def root():
+class Item(BaseModel):
+    name: str
+
+@app.post("/api")
+async def index(item: Item):
+    nick = item.dict()
+    print(nick)
+    url1 = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + parse.quote(nick['name'])
+    response = requests.get(url1, headers={'X-Riot-Token': token})
+    puuid = response.json()['puuid']
+    url2 = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + response.json()['id']
+    response2 = requests.get(url2, headers={'X-Riot-Token': token})
+
     if response.status_code == 200:
-        #변수 초기화1
+        # 변수 초기화1
         information = json.loads(response2.text)[0]
-        #티어
+        # 티어
         nickname = information['tier'][0] + information['tier'][1:].lower()
-        #세부 랭크(숫자)
+        # 세부 랭크(숫자)
         number = 0
-        if information['rank'] == "IV": number = 4
-        elif information['rank'] == "III": number = 3
-        elif information['rank'] == "II": number = 2
-        else: number = 1
+        if information['rank'] == "IV":
+            number = 4
+        elif information['rank'] == "III":
+            number = 3
+        elif information['rank'] == "II":
+            number = 2
+        else:
+            number = 1
         name = response.json()["name"]
-        gameId = requests.get(f"https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids",headers={'X-Riot-Token': token}).json()
+        gameId = requests.get(f"https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids",
+                              headers={'X-Riot-Token': token}).json()
         k, d, a = 0, 0, 0
         visionScore = 0
         averageKilRate = []
@@ -51,8 +65,9 @@ async def root():
         averageCS = []
 
         for i in gameId:
-            #변수 초기화2
-            gameData = requests.get(f"https://asia.api.riotgames.com/lol/match/v5/matches/{i}",headers={"X-Riot-Token": token}).json()
+            # 변수 초기화2
+            gameData = requests.get(f"https://asia.api.riotgames.com/lol/match/v5/matches/{i}",
+                                    headers={"X-Riot-Token": token}).json()
             playerwin = True
             winTeamKills, loseTeamKills = 0, 0
             targetKills, targetAssists = 0, 0
@@ -60,32 +75,35 @@ async def root():
 
             for j in gameData['info']['participants']:
                 # 블루팀 레드팀 킬 나누기(bk가 블루팀이라는건 보장하지 않지만, 나누기만 하면 장땡)
-                if j['win']: winTeamKills += j['kills']
-                else: loseTeamKills += j['kills']
+                if j['win']:
+                    winTeamKills += j['kills']
+                else:
+                    loseTeamKills += j['kills']
 
                 if j['summonerName'].lower() == name.lower():
-                    #이겼는지 여부
+                    # 이겼는지 여부
                     playerwin = j['win']
-                    #평균 cs
-                    csPerMinuite = round((j['totalMinionsKilled'] + j['neutralMinionsKilled']) / round((gameData['info']['gameDuration'] / 60), 1), 1)
-                    #킬관여율
+                    # 평균 cs
+                    csPerMinuite = round((j['totalMinionsKilled'] + j['neutralMinionsKilled']) / round(
+                        (gameData['info']['gameDuration'] / 60), 1), 1)
+                    # 킬관여율
                     targetKills += j['kills']
                     targetAssists += j['assists']
-                    #KDA
+                    # KDA
                     k += j['kills']
                     d += j['deaths']
                     a += j['assists']
-                    #시야점수
+                    # 시야점수
                     visionScore += j['visionScore']
-                    #데스 당 딜량
+                    # 데스 당 딜량
                     try:
                         deathPerDamage.append(round(j['totalDamageDealtToChampions'] / j['deaths']))
                     except:
                         deathPerDamage.append(j['totalDamageDealtToChampions'])
-            #평균 cs append
+            # 평균 cs append
             averageCS.append(csPerMinuite)
 
-            #승리팀일시 winTeam으로, 패배팀일시 loseTeam로
+            # 승리팀일시 winTeam으로, 패배팀일시 loseTeam로
             if playerwin:
                 # 0/n/0박았을시 에러나기때문에 try 사용
                 try:
@@ -101,9 +119,12 @@ async def root():
 
         return {"name": "닉네임: {}".format(response.json()["name"]),
                 "tier": f"티어: {nickname} {number}",
-                "kda": f"평균 KDA: {round((k + a ) / d, 2)}",
-                "vision": f"평균 시야점수: {visionScore/ 20}",
+                "kda": f"평균 KDA: {round((k + a) / d, 2)}",
+                "vision": f"평균 시야점수: {visionScore / 20}",
                 "killRate": f"평균 킬관여율: {round(sum(averageKilRate) / len(averageKilRate))}%",
-                "cs" : f"평균 cs: {round(sum(averageCS) / len(averageCS), 1)}",
+                "cs": f"평균 cs: {round(sum(averageCS) / len(averageCS), 1)}",
                 "deathPerDamage": f"데스 당 딜량: {round(sum(deathPerDamage) / len(deathPerDamage))}"
                 }
+
+
+
